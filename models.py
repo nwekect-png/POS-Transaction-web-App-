508d49470caa9afe214012f61ddb276c28c1143f
nwekect-png <nwekect@gmail.com>
2026-08-19 19:54:24 -0700

    Prepare POS app for deployment

--- models.py
+++ models.py
@@ -2,7 +2,9 @@ from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from extensions import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 # ============================================================
